2c9772831f711534e4c8c167ef9a6089ba1cc50e
moonlightvg <vladislavworkstation@gmail.com>
2026-08-05 13:59:04 +0200

    Initial commit: структура проекта

---
+++ locators/login_locators.py
@@ -0,0 +1,9 @@
+class LoginLocators:
+    """Локаторы для страницы логина"""
+    # Поля ввода
+    EMAIL_INPUT = "[data-qa-id='login_email_input']"
+    PASSWORD_INPUT = "[data-qa-id='login_password_input']"
+    # Кнопки
+    LOGIN_BUTTON = "button[type='submit']"
+    # Регистрация
+    REGISTER_LINK = ("link", "Зарегистрироваться")
